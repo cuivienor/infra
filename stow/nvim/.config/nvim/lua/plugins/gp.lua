f20a73e03afb868de7b2906fb0fe4694ed76b123
Peter Petrov <peter@petrovs.io>
2025-05-22 06:19:22 -0400

    Update gp template to short

--- stow/nvim/.config/nvim/lua/plugins/gp.lua
+++ stow/nvim/.config/nvim/lua/plugins/gp.lua
@@ -1,7 +1,8 @@
 return {
 	"robitx/gp.nvim",
 	config = function()
-		local conf = { openai_api_key = os.getenv("OPEN_API_KEY") }
+		local conf =
+			{ openai_api_key = os.getenv("OPEN_API_KEY"), chat_template = require("gp.defaults").short_chat_template }
 		require("gp").setup(conf)
 	end,
 }
