cdf4e040ced7f4e91643a6bbf8bfd0507c3635a5
Peter Petrov <peter@petrovs.io>
2025-12-26 07:49:37 +0000

    Actually set the shell for the user

--- nixos/hosts/devbox/configuration.nix
+++ nixos/hosts/devbox/configuration.nix
@@ -38,6 +38,7 @@
     cuiv = {
       isNormalUser = true;
       extraGroups = [ "wheel" ];
+      shell = pkgs.zsh;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDeMlFdR2HiSqwxESTKFvgZB4OU/j+taT+dNv96V60Xd cuiv@laptop"
       ];
